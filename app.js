209ed829cc234606c5f28f8e211a0afc91e3fa84
Cihat <copurcihat@gmail.com>
2022-09-28 09:44:52 +0300

    js

---
+++ app.js
@@ -0,0 +1,17 @@
+const   input  = document.querySelector(`.input`);
+
+const  input2   = document.querySelector(`.input2`);
+
+const   btncontainer  = document.querySelector(`.button-container`);
+
+let num1;
+let num2;
+let operator;
+
+let num1done=false;
+let floating = false;
+
+
+btncontainer.onclick=(e)=>{
+
+}
